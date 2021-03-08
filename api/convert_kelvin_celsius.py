f8009f05f44e73ee88c9badf93a2cae0242d7472
tonybutzer <tonybutzer@gmail.com>
2021-03-08 21:12:10 +0000

    automatic git update from Makefile

---
+++ api/convert_kelvin_celsius.py
@@ -0,0 +1,143 @@
+
+#!/usr/bin/env python
+# coding: utf-8
+
+from time import time
+import xarray as xr
+import boto3
+import os
+
+def _split_full_path(bucket_full_path):
+    if 's3://' in bucket_full_path:
+        bucket_full_path=bucket_full_path.replace('s3://','')
+    print(bucket_full_path)
+    bucket = bucket_full_path.split('/')[0]
+    bucket_filepath = '/'.join(bucket_full_path.split('/')[1:])
+    return (bucket, bucket_filepath)
+
+
+
+
+def s3_push_delete_local(local_file, bucket_full_path):
+        s3 = boto3.client('s3')
+        with open(local_file, "rb") as f:
+            (bucket, bucket_filepath) = _split_full_path(bucket_full_path)
+            s3.upload_fileobj(f, bucket, bucket_filepath)
+        os.remove(local_file)
+
+
+
+
+def write_GeoTif_like(templet_tif_file, output_ndarry, output_tif_file):
+    import rasterio
+    orig = rasterio.open(templet_tif_file)
+    print('write file ', output_tif_file)
+    with rasterio.open(output_tif_file, 'w', driver='GTiff', height=output_ndarry.shape[0],
+                       width=output_ndarry.shape[1], count=1, dtype=output_ndarry.dtype,
+                       crs=orig.crs, transform=orig.transform, nodata=-9999) as dst:
+        dst.write(output_ndarry, 1)
+
+
+
+
+
+def _get_year_month(product, tif):
+    fn = tif.split('/')[-1]
+    fn = fn.replace(product,'')
+    fn = fn.replace('.tif','')
+    print(fn)
+    fn=fn[-3:]
+    return fn
+
+
+
+
+
+def xr_build_cube_concat_ds(tif_list, product):
+
+    start = time()
+    my_da_list =[]
+    year_month_list = []
+    for tif in tif_list:
+        tiffile = tif
+        #print(tiffile)
+        da = xr.open_rasterio(tiffile)
+        my_da_list.append(da)
+        tnow = time()
+        elapsed = tnow - start
+        #print(tif, elapsed)
+        print('.',flush=True)
+        year_month_list.append(_get_year_month(product, tif))
+
+    da = xr.concat(my_da_list, dim='band')
+    da = da.rename({'band':'day'})
+    da = da.assign_coords(day=year_month_list)
+    DS = da.to_dataset(name=product)
+    return(DS)
+
+
+
+
+get_ipython().system(' aws s3 ls dev-et-data/in/DelawareRiverBasin/Temp/Tasavg/1950/')
+
+
+
+
+
+def create_s3_list_of_days(main_prefix, year, temperatureType):
+    output_name = f'{temperatureType}_'
+    the_list = []
+    for i in range(1,366):
+        day = f'{i:03d}'
+        file_object = main_prefix + temperatureType + '/' + str(year) +  '/' + output_name + str(year) + day + '.tif'
+        the_list.append(file_object)
+    return the_list
+
+
+
+def main_runner():
+
+main_bucket_prefix='s3://dev-et-data/in/DelawareRiverBasin/Temp/'
+year='1950'
+temperatureType = 'Tasavg'
+
+tif_list = create_s3_list_of_days(main_bucket_prefix, year, temperatureType)
+
+
+tif_list_pruned = tif_list[0:364:30]
+ds = xr_build_cube_concat_ds(tif_list_pruned, temperatureType)
+
+
+for i in range(0,ds.dims['day']):
+    print(ds['Tasavg'][i]['day'])
+
+ds = ds - 273.15 # convert data array xarray.DataSet from Kelvin to Celsius
+
+
+output_main_prefix='s3://dev-et-data/in/DelawareRiverBasin/TempCelsius/'
+year='1950'
+
+
+get_ipython().system('mkdir -p ./tmp')
+
+write_out_celsius_tifs(output_main_prefix, ds, year, 'Tasavg')
+
+
+import rioxarray
+import rasterio
+def write_out_celsius_tifs(main_prefix, ds, year, output_name):
+    num_days=ds.dims['day']
+    for i in range(0,(num_days - 1)):
+        dayi = i+1
+        day="{:03d}".format(dayi)
+        s3_file_object = main_prefix + temperatureType + '/' + str(year) +  '/' + output_name + '_' + str(year) + day + '.tif'
+        file_object = './tmp/' + output_name + '_' + str(year) + day + '.tif'
+
+        print(file_object)
+
+        np_array = ds[output_name].isel(day=dayi).values
+#         print(type(np_array))
+        my_template = 's3://dev-et-data/in/DelawareRiverBasin/Temp/Tasavg/1950/Tasavg_1950017.tif'
+        write_GeoTif_like(my_template, np_array, file_object)
+        s3_push_delete_local(file_object, s3_file_object)
+
---
+++ api/kelvin_orchestration.py
@@ -0,0 +1,129 @@
+import time
+import os
+import subprocess
+import re
+
+import yaml
+import json
+import docker
+
+
+# python3 -m pip install -U PyYAML
+
+def return_cpu_load():
+    cpu_load = [x / os.cpu_count() * 100 for x in os.getloadavg()][-1]
+    return cpu_load
+
+
+def _return_mem_stat():
+    # Memory usage
+    total_ram = subprocess.run(['free', '-h'], stdout=subprocess.PIPE).stdout.decode('utf-8')
+    used_free_shared_buf_avail = total_ram.split('\n')[1]
+
+    return  used_free_shared_buf_avail
+
+def return_available_memory():
+    used_free_shared_buf_avail = _return_mem_stat()
+    a = re.split('\s+', used_free_shared_buf_avail)
+    available_memory = a[3].split('G')[0]
+    return float(available_memory)
+
+
+class Etm:
+    def __init__(self, yml_file='etm_run.yml'):
+        print('Etm instaniated', yml_file)
+        self.yml_file = yml_file
+        self._read_yml()
+        self.client = docker.from_env()
+    
+    def __repr__(self):
+        return(json.dumps(etm.etm_parms, indent=2))
+        
+    def _read_yml(self):
+        print(self.yml_file)
+        with open(self.yml_file) as file:
+            self.etm_parms = yaml.full_load(file)
+
+    def MAIN_etm_runner(self):
+        products = self.etm_parms['products']
+        start_year = self.etm_parms['start_year']
+        end_year = self.etm_parms['end_year']
+        for product in products:
+            print(product)
+            self._event_loop(start_year, end_year, product)
+
+    def _start_container(self, docker_image, docker_full_cmd, name):
+        container = self.client.containers.run(docker_image, docker_full_cmd, detach=True, auto_remove=True, name=name)
+        # container = self.client.containers.run(docker_image, docker_full_cmd, detach=True, name=name)
+        print ( "CONTAINER is ", container.name)
+        return(container)
+
+    def _start_etm(self, year, product):
+        unmosaicked_input=self.etm_parms['unmosaicked_input']
+        enduser_cog_output=self.etm_parms['enduser_cog_output']
+    
+        cmd_opt = '-i ' + unmosaicked_input + ' -o ' + enduser_cog_output + ' -y ' + year + ' ' + product + ' dummy'
+        print(cmd_opt)
+        cmd = 'python3 api_etm.py '
+        full_cmd = cmd + cmd_opt
+        #print(full_cmd)
+        docker_image =  "tbutzer/etm_docker_image"
+        #print(docker_image)
+        name_c = f'etmv1_{year}'
+        c = self._start_container(docker_image, full_cmd, name_c)
+        print("real name is", c.name, product)
+        print("==="*30)
+
+    def _return_num_containers(self):
+        client = self.client
+        try:
+            running_containers = client.containers.list()
+        except:
+            running_containers=[]
+        return(len(running_containers))
+
+    def _event_loop(self, year_to_process, end_year, product):
+
+        MAX_LOAD_LEVEL = self.etm_parms['max_cpu_percent']
+        MIN_MEMORY_AVAILABLE =  self.etm_parms['min_memory_available']
+        MAX_CONCURRENT_CONTAINERS = self.etm_parms['max_concurrent_containers']
+
+        print(f' {year_to_process} <= {end_year}:')
+        while year_to_process <= end_year:
+            print('sleeping for 30 .... ')
+            time.sleep(30)
+
+            mem_avail = return_available_memory()
+            cpu_load = return_cpu_load()
+            num_running_containers = self._return_num_containers()
+            print(f'mem={mem_avail}, cpu={cpu_load}, num_containers={num_running_containers}')
+
+            cpu = False
+            if (cpu_load < MAX_LOAD_LEVEL):
+                print("CPU is FINE")
+                cpu=True
+
+            mem = False
+            if mem_avail > MIN_MEMORY_AVAILABLE:
+                print("MEM is FINE")
+                mem = True
+
+            containers = False
+            if num_running_containers < MAX_CONCURRENT_CONTAINERS:
+                containers = True
+            else:
+                print("MAX Container Level Reached - Chill :-)")
+
+
+            if (mem and cpu and containers):
+                print("OK to Launch")
+                self._start_etm(str(year_to_process), product) ### start mosaic container
+                print("starting year", year_to_process)
+                year_to_process = year_to_process + 1
+
+
+etm=Etm()
+print(etm)
+
+etm.MAIN_etm_runner()
+
